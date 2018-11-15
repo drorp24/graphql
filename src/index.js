71f43f7699b468fa18601aa78ff81a71ec342904
Dror <drorp24@gmail.com>
2018-11-15 23:25:17 +0200

    Removing express altogether

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import cors from 'cors'
 import 'dotenv/config'
-import express from 'express'
 import { ApolloServer } from 'apollo-server'
 
 import schema from './schema'
 import resolvers from './resolvers'
 import models from './models'
-
-const app = express()
-
-app.use(cors())
 
 const server = new ApolloServer({
   typeDefs: schema,
